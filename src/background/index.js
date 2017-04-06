f6d2f223c1e81aebcc85aa88c246a25bacb03b3c
Benoit Bar <benoit.bar@gandi.net>
2017-04-06 16:38:58 +0200

    Initial commit

---
+++ src/background/index.js
@@ -0,0 +1,45 @@
+
+var browser = chrome || browser;
+
+function openIncognito(url) {
+  console.log("Attempting to open URL incognito: ", url);
+  browser.windows.getAll({}, function (wins) {
+    var incognitoWins = wins.filter(function (w) { return w.incognito; });
+    var focussedWins = wins.filter(function (w) { return w.focused; });
+
+    var state = (focussedWins.length > 0)
+      ? (focussedWins[0].state || "normal")
+      : "normal";
+
+    if (incognitoWins.length > 0 && typeof incognitoWins[0].id !== "undefined") {
+      browser.tabs.create({ url: url, windowId: incognitoWins[0].id });
+    } else {
+      try {
+        browser.windows.create(
+          {
+            url: url,
+            incognito: true,
+            // May not be able to use `state` as it is supported
+            // only in Chrome 41 and beyond.
+            state: state
+          }
+        );
+      } catch (e) {
+        console.log("Trying without `state`");
+        browser.windows.create(
+          {
+            url: url,
+            incognito: true
+          }
+        );
+      }
+    }
+  });
+}
+
+browser.extension.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    openIncognito(request.url);
+    sendResponse(true);
+  }
+);
---
+++ src/linkognito.js
@@ -0,0 +1,23 @@
+var browser = chrome || browser;
+var links = document.querySelectorAll('a');
+
+const callback = function(url) {
+  return function(resp) {
+    if (resp) {
+        console.log('Opened URL: ', url, ' in a new incognito window.');
+    } else {
+        console.error('Failed to open the URL: ', url);
+    }
+  }
+}
+
+for (var i = 0; i < links.length; i++) {
+  links.item(i).addEventListener('click', function(evt) {
+    var targetElement = evt.target;
+
+    if (targetElement !== null) {
+      console.log(browser);
+      browser.runtime.sendMessage({ url: evt.currentTarget.href }, callback(targetElement.href));
+    }
+  }, false)
+}
